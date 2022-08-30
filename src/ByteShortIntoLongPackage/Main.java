a2610b639ac5a3ede93e45045f6f41175bbed790
Josh Snyder <joshrsnyder@gmail.com>
2022-08-30 15:44:42 -0500

    learning java!

---
+++ src/ByteShortIntoLongPackage/Main.java
@@ -0,0 +1,12 @@
+package ByteShortIntoLongPackage;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        int myValue = 10000;
+
+        int myMinIntValue = Integer.MIN_VALUE;
+        int myMaxIntValue = Integer.MAX_VALUE;
+    }
+}
